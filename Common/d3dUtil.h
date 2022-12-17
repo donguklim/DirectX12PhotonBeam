e706587472259d8719d52a362d46c860411ad300
dlimdlim <dlim7272@gmail.com>
2022-12-17 15:01:40 +0900

    set microsoft direcx headers as external header

--- Common/d3dUtil.h
+++ Common/d3dUtil.h
@@ -25,7 +25,7 @@
 #include <fstream>
 #include <sstream>
 #include <cassert>
-#include "../directx-headers/directx/d3dx12.h"
+#include "d3dx12.h"
 #include "DDSTextureLoader.h"
 #include "MathHelper.h"
 
