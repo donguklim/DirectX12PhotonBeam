76053c4210a8f8963ef6789636eb75e3bfb061a5
dlimdlim <dlim7272@gmail.com>
2022-12-18 13:49:17 +0900

    show wire only when keyboard key 1 is pressed

--- PhotonBeam/PhotonBeamApp.cpp
+++ PhotonBeam/PhotonBeamApp.cpp
@@ -692,7 +692,6 @@ void PhotonBeamApp::BuildPSOs()
 		mShaders["opaquePS"]->GetBufferSize()
 	};
 	opaquePsoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
-    opaquePsoDesc.RasterizerState.FillMode = D3D12_FILL_MODE_WIREFRAME;
 	opaquePsoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
 	opaquePsoDesc.DepthStencilState = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
 	opaquePsoDesc.SampleMask = UINT_MAX;
