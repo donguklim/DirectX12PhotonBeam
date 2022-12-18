2cbb120d5a0d336050a52e469064b0710deb11d6
dlimdlim <dlim7272@gmail.com>
2022-12-19 03:44:14 +0900

    add light functions

--- PhotonBeam/Shaders/color.hlsl
+++ PhotonBeam/Shaders/color.hlsl
@@ -60,6 +60,28 @@ struct VertexOut
     float2 TexC : TEXCOORD;
 };
 
+float3 computeDiffuse(MaterialData mat, float3 lightDir, float3 normal)
+{
+    // Lambertian
+    float dotNL = max(dot(normal, lightDir), 0.0);
+    return mat.pbrBaseColorFactor.xyz * dotNL;
+}
+
+float3 computeSpecular(MaterialData mat, float3 viewDir, float3 lightDir, float3 normal)
+{
+    // Compute specular only if not in shadow
+    const float kPi = 3.14159265;
+    const float kShininess = 60.0;
+
+    // Specular
+    const float kEnergyConservation = (2.0 + kShininess) / (2.0 * kPi);
+    float3        V = normalize(-viewDir);
+    float3        R = reflect(-lightDir, normal);
+    float       specular = kEnergyConservation * pow(max(dot(V, R), 0.0), kShininess);
+
+    return float3(specular, specular, specular);
+}
+
 VertexOut VS(VertexIn vin)
 {
 	VertexOut vout;
