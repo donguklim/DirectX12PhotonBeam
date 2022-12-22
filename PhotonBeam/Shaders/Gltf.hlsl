06898e2c3f0d2ef4232bfa87d72d0bc9337dcfd4
dlimdlim <dlim7272@gmail.com>
2022-12-22 10:30:57 +0900

    make separate shader file for gltf related functions and structures

---
+++ PhotonBeam/Shaders/Gltf.hlsl
@@ -0,0 +1,32 @@
+
+struct MaterialData
+{
+    float4 pbrBaseColorFactor;
+    float3 emissiveFactor;
+    int  pbrBaseColorTexture;
+    float metallic;
+    float roughness;
+    uint   padding[2];
+};
+
+float3 computeDiffuse(MaterialData mat, float3 lightDir, float3 normal)
+{
+    // Lambertian
+    float dotNL = max(dot(normal, lightDir), 0.0);
+    return mat.pbrBaseColorFactor.xyz * dotNL;
+}
+
+float3 computeSpecular(MaterialData mat, float3 viewDir, float3 lightDir, float3 normal)
+{
+    // Compute specular only if not in shadow
+    const float kPi = 3.14159265;
+    const float kShininess = 60.0;
+
+    // Specular
+    const float kEnergyConservation = (2.0 + kShininess) / (2.0 * kPi);
+    float3        V = normalize(-viewDir);
+    float3        R = reflect(-lightDir, normal);
+    float       specular = kEnergyConservation * pow(max(dot(V, R), 0.0), kShininess);
+
+    return float3(specular, specular, specular);
+}
--- PhotonBeam/Shaders/color.hlsl
+++ PhotonBeam/Shaders/color.hlsl
@@ -1,19 +1,6 @@
-//***************************************************************************************
-// color.hlsl by Frank Luna (C) 2015 All Rights Reserved.
-//
-// Transforms and colors geometry.
-//***************************************************************************************
- 
 
-struct MaterialData
-{
-    float4 pbrBaseColorFactor;
-    float3 emissiveFactor;
-    int  pbrBaseColorTexture;
-    float metallic;
-    float roughness;
-    uint   padding[2];
-};
+#include "gltf.hlsl"
+
 
 StructuredBuffer<MaterialData> gMaterialData : register(t0, space1);
 
@@ -23,6 +10,7 @@ cbuffer cbPerObject : register(b0)
 	float4x4 gWorld;
     uint materialIndex;
 };
+
 
 cbuffer cbPass : register(b1)
 {
@@ -62,27 +50,6 @@ struct VertexOut
     float2 TexC : TEXCOORD;
 };
 
-float3 computeDiffuse(MaterialData mat, float3 lightDir, float3 normal)
-{
-    // Lambertian
-    float dotNL = max(dot(normal, lightDir), 0.0);
-    return mat.pbrBaseColorFactor.xyz * dotNL;
-}
-
-float3 computeSpecular(MaterialData mat, float3 viewDir, float3 lightDir, float3 normal)
-{
-    // Compute specular only if not in shadow
-    const float kPi = 3.14159265;
-    const float kShininess = 60.0;
-
-    // Specular
-    const float kEnergyConservation = (2.0 + kShininess) / (2.0 * kPi);
-    float3        V = normalize(-viewDir);
-    float3        R = reflect(-lightDir, normal);
-    float       specular = kEnergyConservation * pow(max(dot(V, R), 0.0), kShininess);
-
-    return float3(specular, specular, specular);
-}
 
 VertexOut VS(VertexIn vin)
 {
@@ -99,6 +66,7 @@ VertexOut VS(VertexIn vin)
     
     return vout;
 }
+
 
 float4 PS(VertexOut pin) : SV_Target
 {
@@ -117,5 +85,3 @@ float4 PS(VertexOut pin) : SV_Target
 
     return pow(float4(lightIntensity * (diffuse + specular), 1), gammaVec);
 }
-
-
