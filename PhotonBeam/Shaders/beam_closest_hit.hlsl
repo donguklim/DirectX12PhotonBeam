7709028611359990d9d1b52ba712a97fb4412444
dlimdlim <dlim7272@gmail.com>
2022-12-22 22:56:34 +0900

    working on

--- PhotonBeam/Shaders/beam_closest_hit.hlsl
+++ PhotonBeam/Shaders/beam_closest_hit.hlsl
@@ -15,18 +15,17 @@ StructuredBuffer<float3> g_vertices : register(t2, space0);
 StructuredBuffer<float3> g_normals : register(t3, space0);
 StructuredBuffer<float2> g_texcoordinate : register(t4, space0);
 
-StructuredBuffer<GltfShadeMaterial> g_material : register(t5, space0);
-StructuredBuffer<PrimMeshInfo> g_meshInfo : register(t6, space0);
+StructuredBuffer<GltfShadeMaterial> g_materials : register(t5, space0);
+StructuredBuffer<PrimMeshInfo> g_meshInfos : register(t6, space0);
 
-Texture2D g_diffuseMap[] : register(t0, space1);
-
-
-
-
+Texture2D g_texturesMap[] : register(t0, space1);
 
 
 bool randomScatterOccured(inout BeamHitPayload prd, const in float3 world_position) {
+    
+    prd.scatterIndex = 0;
     float min_extinct = min(min(pc_ray.airExtinctCoff.x, pc_ray.airExtinctCoff.y), pc_ray.airExtinctCoff.z);
+    
     if (min_extinct <= 0.001)
         return false;
 
@@ -41,7 +40,7 @@ bool randomScatterOccured(inout BeamHitPayload prd, const in float3 world_positi
     }
 
     prd.rayOrigin = prd.rayOrigin + prd.rayDirection * airScatterAt;
-    prd.instanceIndex = -1;
+    prd.scatterIndex = 1;
 
     float3 albedo = pc_ray.airScatterCoff / pc_ray.airExtinctCoff;
     float absorptionProb = 1.0 - max(max(albedo.x, albedo.y), albedo.z);
@@ -60,7 +59,96 @@ bool randomScatterOccured(inout BeamHitPayload prd, const in float3 world_positi
 
 
 [shader("closesthit")] 
-void ClosestHit(inout BeamHitPayload prd, Attributes attrib)
+void ClosestHit(inout BeamHitPayload prd, Attributes attribs)
 {
- 
+    PrimMeshInfo meshInfo = g_meshInfos[InstanceID()];
+    prd.instanceID = InstanceID();
+
+    // Getting the 'first index' for this mesh (offset of the mesh + offset of the triangle)
+    uint indexOffset = (pinfo.indexOffset / 3) + PrimitiveIndex();
+    uint vertexOffset = pinfo.vertexOffset;           // Vertex offset as defined in glTF
+    uint materialIndex = max(0, pinfo.materialIndex);  // material of primitive mesh
+
+    float3  vertices = Vertices(sceneDesc.vertexAddress);
+    uint3   indices = Indices(sceneDesc.indexAddress);
+    float3   normals = Normals(sceneDesc.normalAddress);
+    float2 texCoords = TexCoords(sceneDesc.uvAddress);
+    Materials materials = Materials(sceneDesc.materialAddress);
+
+    // Getting the 3 indices of the triangle (local)
+    uint3 triangleIndex = g_indices[indexOffset];
+    triangleIndex += uint3(vertexOffset, vertexOffset, vertexOffset);  // (global)
+
+    const float3 barycentrics = float3(1.0 - attribs.bary.x - attribs.bary.y, attribs.bary.x, attribss.bary.y);
+
+    // Vertex of the triangle
+    const float3 pos0 = g_vertices.v[triangleIndex.x];
+    const float3 pos1 = g_vertices.v[triangleIndex.y];
+    const float3 pos2 = g_vertices.v[triangleIndex.z];
+    const float3 position = pos0 * barycentrics.x + pos1 * barycentrics.y + pos2 * barycentrics.z;
+    const float3 world_position = float3(gl_ObjectToWorldEXT * float3(position, 1.0));
+
+    float3 world_position = mul(ObjectToWorld3x4(), float4(position, 1.0f));
+
+    // if random scatter occured in media before hitting a surface, return
+    if (randomScatterOccured(world_position))
+        return;
+
+    // Normal
+    const float3 nrm0 = normals.n[triangleIndex.x];
+    const float3 nrm1 = normals.n[triangleIndex.y];
+    const float3 nrm2 = normals.n[triangleIndex.z];
+    float3       normal = normalize(nrm0 * barycentrics.x + nrm1 * barycentrics.y + nrm2 * barycentrics.z);
+
+    const float3 world_normal = normalize(mul((float3x3) ObjectToWorld3x4(), normal));
+    // const float3 geom_normal = normalize(cross(pos1 - pos0, pos2 - pos0));
+
+
+    // TexCoord
+    const float2 uv0 = texCoords.t[triangleIndex.x];
+    const float2 uv1 = texCoords.t[triangleIndex.y];
+    const float2 uv2 = texCoords.t[triangleIndex.z];
+    const float2 texcoord0 = uv0 * barycentrics.x + uv1 * barycentrics.y + uv2 * barycentrics.z;
+
+    // https://en.wikipedia.org/wiki/Path_tracing
+    // Material of the object
+    GltfShadeMaterial material = g_materials[matIndex];
+    float3 rayOrigin = world_position;
+    prd.hitNormal = world_normal;
+
+    float cos_theta = dot(-prd.rayDirection, world_normal);
+    if (cos_theta <= 0)
+    {
+        prd.rayOrigin = rayOrigin;
+        prd.weight = float3(0.0);
+        return;
+    }
+
+    float3 rayDirection = microfacetReflectedLightSampling(prd.seed, prd.rayDirection, world_normal, mat.roughness);
+
+    // rays reflected toward inside of the surface are considered to be absorbd
+    if (dot(world_normal, rayDirection) <= 0)
+    {
+        prd.rayOrigin = rayOrigin;
+        prd.rayDirection = rayDirection;
+        prd.weight = float3(0.0);
+        return;
+    }
+
+    float3  albedo = material.pbrBaseColorFactor.xyz;
+
+    if (material.pbrBaseColorTexture > -1)
+    {
+        uint txtId = material.pbrBaseColorTexture;
+        albedo *= texture(texturesMap[nonuniformEXT(txtId)], texcoord0).xyz;
+    }
+
+    float3 material_f = pdfWeightedGltfBrdf(-prd.rayDirection, rayDirection, world_normal, albedo, mat.roughness, mat.metallic);
+    float rayLength = length(prd.rayOrigin - world_position);
+
+    prd.rayOrigin = rayOrigin;
+    prd.rayDirection = rayDirection;
+    prd.weight = material_f * cos_theta * exp(-pcRay.airExtinctCoff * rayLength);
+
+    return;
 }
--- PhotonBeam/Shaders/beam_common.hlsl
+++ PhotonBeam/Shaders/beam_common.hlsl
@@ -5,9 +5,9 @@ struct BeamHitPayload
 	float3 rayOrigin;
 	uint seed;
 	float3 rayDirection;
-	int  instanceIndex;
+	uint  instanceID;
 	float3 weight;
-	uint padding1;
+	uint scatterIndex; // index of the material whre beam scatter has occured. 0 means no scattering 1 means air scattering
 	float3 hitNormal;
 	uint padding2;
 };
