2ea59617ed5c790a3475a28ad9bf239072cdeb31
dlimdlim <dlim7272@gmail.com>
2022-12-30 14:30:42 +0900

    fix error

--- PhotonBeam/Shaders/util/HlslCompat.h
+++ PhotonBeam/Shaders/util/HlslCompat.h
@@ -4,6 +4,10 @@
 #ifndef HLSLCOMPAT_H
 #define HLSLCOMPAT_H
 
+typedef uint3 XMUINT2;
+typedef uint3 XMUINT3;
+typedef uint4 XMUINT4;
+
 typedef float2 XMFLOAT2;
 typedef float3 XMFLOAT3;
 typedef float4 XMFLOAT4;
