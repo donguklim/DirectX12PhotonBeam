3eae312098ae9b1ccb533828a054325d329bd0fc
dlimdlim <dlim7272@gmail.com>
2023-01-03 16:35:44 +0900

    working on compute shader

--- PhotonBeam/Shaders/BeamTracing/ResetBeamASBuffer.hlsl
+++ PhotonBeam/Shaders/BeamTracing/ResetBeamASBuffer.hlsl
@@ -1,11 +1,32 @@
 
-#ifndef PHOTONBEAM_BEAM_GEN
-#define PHOTONBEAM_BEAM_GEN
+#ifndef PHOTONBEAM_BEAM_AS_INSTANCE_BUFFER
+#define PHOTONBEAM_RESET_BEAM_AS_INSTANCE_BUFFER
+
+#define N 256
 
 #include "..\util\RayTracingSampling.hlsli"
 #include "..\RaytracingHlslCompat.h"
 
-void main()
+cbuffer bufferSize : register(b0)
 {
-
+	uint maxNumSubBeams;
 }
+
+RWStructuredBuffer<ShaderRayTracingTopASInstanceDesc> g_photonBeamsTopAsInstanceDescs : register(u0, space0);
+
+
+[numthreads(N, 1, 1)]
+void main(int3 dispatchThreadID : SV_DispatchThreadID)
+{
+	const ShaderRayTracingTopASInstanceDesc empty = = (ShaderRayTracingTopASInstanceDesc)0;
+
+	uint i = dispatchThreadID.x;
+	while (i < bufferSize.maxNumSubBeams)
+	{
+		g_photonBeamsTopAsInstanceDescs[i] = empty;
+
+		i += N;
+	}
+}
+
+#endif
